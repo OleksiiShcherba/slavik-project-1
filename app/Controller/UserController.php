bd321dbab782ae9ca0f6220239197bedeae74d42
Oleksii <shcherbaalex77@gmail.com>
2024-01-24 05:41:27 -0500

    added route system

---
+++ app/Controller/UserController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Controller;
+
+class UserController
+{
+	public function getUser(){
+		var_dump("HELLO");
+	}	
+}
--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php
-
 require('vendor/autoload.php');
 
+use App\Controller\UserController;
+use Symfony\Component\Routing\Generator\UrlGenerator;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
 
-$user = new App\User();
-$user->callMe();
+$route = new Route('user', ['_controller' => UserController::class, '_function' => 'getUser']);
+
+$routes = new RouteCollection();
+$routes->add('getUser', $route);
+
+$context = new RequestContext();
+$matcher = new UrlMatcher($routes, $context);
+$parameters = $matcher->match($_SERVER['REQUEST_URI']);
+var_dump($parameters);
