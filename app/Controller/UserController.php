95ff56053d7ea5c076a9881282be1fecfc7926da
Oleksii <shcherbaalex77@gmail.com>
2024-01-25 13:00:52 -0500

    25.01.24

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -2,9 +2,30 @@
 
 namespace App\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+
 class UserController
 {
-	public function getUser(){
-		var_dump("HELLO");
-	}	
+	public function createUser(Request $request){
+		$html = "<!DOCTYPE html>
+		<html>
+		<head>
+		    <title>My Webpage</title>
+		</head>
+		<body>
+		    <h1>Hello, world! Hello " . $request->query->get('username') . "</h1>
+		    <form action='http://localhost/new/user'>
+		        <input type='text' name='username' />
+		        <button type='submit'>Greet me</button>
+		    </form>
+		</body>
+		</html>";
+
+		echo($html);
+	}
+
+
+	public function newUser(Request $request){
+		var_dump($request->query->get('username'));
+	}
 }
--- index.php
+++ index.php
@@ -7,13 +7,28 @@ use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\Routing\Route;
 use Symfony\Component\Routing\RouteCollection;
+use Symfony\Component\HttpFoundation\Request;
 
-$route = new Route('user', ['_controller' => UserController::class, '_function' => 'getUser']);
+try {
+	$routes = new RouteCollection();
 
-$routes = new RouteCollection();
-$routes->add('getUser', $route);
+	$route = new Route('/user', ['_controller' => UserController::class, '_function' => 'createUser']);
+	$routes->add('getUser', $route);
 
-$context = new RequestContext();
-$matcher = new UrlMatcher($routes, $context);
-$parameters = $matcher->match($_SERVER['REQUEST_URI']);
-var_dump($parameters);
+	$route = new Route('/new/user', ['_controller' => UserController::class, '_function' => 'newUser']);
+	$routes->add('newUser', $route);
+
+	$route = new Route('', ['_controller' => UserController::class, '_function' => 'basic']);
+	$routes->add('basic', $route);
+
+
+	$request = Request::createFromGlobals();
+	$context = new RequestContext();
+
+	$matcher = new UrlMatcher($routes, $context);
+	$parameters = $matcher->matchRequest($request);
+
+	(new $parameters['_controller'])->{$parameters['_function']}($request);
+} catch (\Throwable $e) {
+	var_dump($e->getMessage());
+}
