e36fcb5893bfba3d64cbd18f3c420175f69863dd
Oleksii <shcherbaalex77@gmail.com>
2024-02-09 04:28:45 -0500

    added get last insert id

--- app/Controller/UserController.php
+++ app/Controller/UserController.php
@@ -10,18 +10,18 @@ class UserController
     public function createUser(Request $request)
     {
         $html = "<!DOCTYPE html>
-		<html>
-		<head>
-		    <title>My Webpage</title>
-		</head>
-		<body>
-		    <h1>Hello, world! Hello " . $request->query->get('username') . "</h1>
-		    <form action='http://localhost/new/user'>
-		        <input type='text' name='username' />
-		        <button type='submit'>Greet me</button>
-		    </form>
-		</body>
-		</html>";
+        <html>
+        <head>
+            <title>My Webpage</title>
+        </head>
+        <body>
+            <h1>Hello, world! Hello " . $request->query->get('username') . "</h1>
+            <form action='http://localhost/new/user'>
+                <input type='text' name='username' />
+                <button type='submit'>Greet me</button>
+            </form>
+        </body>
+        </html>";
 
         echo ($html);
     }
@@ -30,12 +30,12 @@ class UserController
     {
         $db = new DB();
         $save_user = $db->saveUser($request->query->get('username'));
+        var_dump($save_user);
 
         if ($save_user) {
             echo ("SAVED");
         } else {
             echo ("FAILED");
         }
-
     }
 }
--- app/Services/DB.php
+++ app/Services/DB.php
@@ -11,35 +11,25 @@ class DB
 
     public function connect()
     {
-        // Create connection
         $conn = mysqli_connect($this->servername, $this->username, $this->password, $this->database);
 
-        // Check connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
 
         return $conn;
-
-        // $result = $conn->query('SELECT * FROM Users WHERE `id` = 2');
-
-        // if ($result->num_rows > 0) {
-        //     // output data of each row
-        //     while ($row = $result->fetch_assoc()) {
-        //         echo "id: " . $row["id"] . " - Name: " . $row["username"] . "\n";
-        //     }
-        // } else {
-        //     echo "0 results";
-        // }
-
-        // mysqli_close($conn);
     }
 
     public function saveUser(string $username)
     {
         $connection = $this->connect();
 
-        $result = $connection->query("INSERT INTO Users (id, username) VALUES (" . time() . ", '" . $username . "')");
+        $result = $connection->query("INSERT INTO Users (username) VALUES ('" . $username . "');");
+
+        $last_inserted_id = mysqli_insert_id($connection);
+
+        $result = $connection->query("SELECT * FROM Users WHERE id = $last_inserted_id");
+        $result = mysqli_fetch_assoc($result);
 
         $connection->close();
 
